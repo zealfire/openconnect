ad5371ad9c27d682553b3bbe7aa1a933ee998ea1
zealfire <iamalok185@gmail.com>
2017-07-08 23:37:49 +0530

    adding initial version of script.

--- openconnect.sh
+++ openconnect.sh
@@ -1,1 +1,11 @@
+cat > vpn << EOF
 #!/usr/bin/expect -f
+set timeout -1
+spawn openconnect GATEWAY
+expect "Enter 'yes' to accept, 'no' to abort; anything else to view:" {send -- "yes\r"}
+expect "Username:" {send -- "USERNAME\r"}
+expect "Password:" {send -- "PASSWORD\r"}
+expect eof
+EOF
+chmod +x vpn
+sudo mv vpn /usr/local/bin/
