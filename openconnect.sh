0c98ebb48f355175efcfd170c479ecb8bf9ad9df
zealfire <iamalok185@gmail.com>
2017-07-09 13:08:18 +0530

    spawning openconnect command with sudo.

--- openconnect.sh
+++ openconnect.sh
@@ -1,7 +1,8 @@
 cat > vpn << EOF
 #!/usr/bin/expect -f
 set timeout -1
-spawn openconnect GATEWAY
+spawn sudo openconnect GATEWAY
+expect "*password*:" {send -- "UBUNTUPASSWORD\r"}
 expect "Enter 'yes' to accept, 'no' to abort; anything else to view:" {send -- "yes\r"}
 expect "Username:" {send -- "USERNAME\r"}
 expect "Password:" {send -- "PASSWORD\r"}
